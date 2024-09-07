c3eec88f51b11329181c33b58e55b53251b6ab41
Vania2007 <momotis2007@gmail.com>
2024-09-07 14:56:35 +0300

    hw-1

---
+++ 2.1.php
@@ -0,0 +1,10 @@
+<?php
+$year = 2024; //integer
+$is_hungry = true; //boolean
+$price = 25.40; //float
+$favourite_movie = "Таксі"; //string
+
+echo "Рік: $year\n<br/>";
+echo "Голодання: $is_hungry\n<br/>";
+echo "Ціна: $price\n<br/>";
+echo "Улюблений фільм: $favourite_movie";
